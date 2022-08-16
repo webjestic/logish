b303afb347b7f7ae9a9f8c8ef536adffed086861
Michael Newlon <michael.newlon@tn.gov>
2022-08-16 09:53:56 -0400

    enhance: imperoved files in example/ for more use

--- examples/exampleB.mjs
+++ examples/exampleB.mjs
@@ -1,13 +1,8 @@
 
 import { Logish } from './../src/logish.js'
 
-
-
 export function exampleB () {
     const log = new Logish()
     log.setNamespace('example:B')
-
-    log.info('Goodbye from exampleB .')
-    log.error('Exception or error call')
-    log.fatal('Fatal brings down the system')
+    log.info('Log already created once, and will use that configruation.')
 }
